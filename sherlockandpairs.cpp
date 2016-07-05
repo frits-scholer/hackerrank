1ac4abdd43ba387411c94795fe0ab69e98640083
frits-scholer <frits.scholer@gmail.com>
2016-07-05 14:56:26 +0200

    added sherlock and pairs

---
+++ sherlockandpairs.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+  //input
+  int T;
+  cin >> T;
+  long N;
+  for (int i=0;i<T;i++) {
+    cin >> N;
+    long A[N];
+    multiset<long> m;
+    for (auto& a:A) cin >> a;
+    for (auto a:A) m.insert(a);
+    long cur = *begin(m);
+    long S = 0;
+    long curcount=0;
+    for (auto it=begin(m);it != end(m); ++it){
+      if (*it != cur) {S+=curcount*(curcount-1);curcount=0;cur=*it;}
+      curcount++;
+    }
+    S+=curcount*(curcount-1);
+    m.clear();
+    cout << S << endl;
+  }
+}
