96495f33131fdac1bf01470b0e9c0e5a3001c4e4
frits-scholer <frits.scholer@gmail.com>
2016-07-29 13:32:18 +0200

    first version of maxsubarray

--- maxsubarray1.cpp
+++ maxsubarray1.cpp
@@ -1,32 +1,35 @@
 #include <iostream>
-#include <tuple>
+#include <utility>
 using namespace std;
 
-typedef tuple<int, int, int> subarray;
+typedef pair<int, int> subarray;
+#define S(i,j,A) A[j]-A[i-1]
 
-void print_subarray(subarray sa) {
-  cout << "(" << get<0>(sa) << "," << get<1>(sa) << ")=>" << get<2>(sa) << endl;
+void print_subarray(subarray sa, int *acc) {
+  cout << "(" << sa.first << "," << sa.second << ")=>" << S(sa.first, sa.second, acc) << endl;
 }
 
 int main() {
-  int A[] {13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7};
-  int acc[16];
-  int low, high, sum;
-  subarray maxSj = make_tuple(0,0,A[0]);
-  print_subarray(maxSj);
+  int A[] {0,13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7};
+  int acc[17];
+  subarray maxSj = make_pair(1,1);
+  int Simaxj=A[1];
+  int imax=1;
   acc[0]=A[0];
-  for (int j=1;j<16;j++) {
+  acc[1]=A[1];
+  for (int j=2;j<16;j++) {
     acc[j]=acc[j-1]+A[j];
-    tie(low, high, sum) = maxSj;
     if (A[j]>0) {
-      if (high == j-1) high = j;
-      else {
-	if (acc[j-1]-acc[high+1] > 0) high = j;
-	else if (A[j]>sum) {low=j;high=j;}
+      int sum = S(maxSj.first, maxSj.second,acc);
+      if (Simaxj < 0) {
+	Simaxj = 0;
+	imax = j;
+      }
+      if (A[j]> sum - Simaxj) {
+	maxSj = make_pair(imax, j);
       }
     }
-    sum = low>0?acc[high]-acc[low-1]:acc[high];
-    maxSj = make_tuple(low, high, sum);
-    print_subarray(maxSj);
+    Simaxj += A[j];
   }
+  print_subarray(maxSj,acc);
 }
