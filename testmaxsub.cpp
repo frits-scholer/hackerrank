f67b366189524b5f5cc2289268776fb8a0b63663
frits-scholer <frits.scholer@gmail.com>
2016-07-31 23:24:36 +0200

    update

--- testmaxsub.cpp
+++ testmaxsub.cpp
@@ -18,26 +18,30 @@ int main() {
     long long acc[n+1];
     subarray maxSj = make_pair(1L,1L);
     long long Simaxj=A[1]%m;
-    long long sum = A[1]%m;
+    long long Smax = A[1]%m;
     long imax=1;
     acc[0]=A[0];
     acc[1]=A[1]%m;
     for (long j=2;j<=n;j++) {
       A[j] %= m;
       acc[j]=(acc[j-1]+A[j])%m;//beware of negatives
-      if (Simaxj + A[j] >= m) {
-	Simaxj = 0;
+      //assume Simaxj is determined
+      if (Simaxj + A[j] >= m) {//determine imax and Simaxj
+	Simaxj = A[j];
 	imax = j;
       }
-      if ((A[j] + Simaxj)%m > sum) {
-	maxSj = make_pair(imax, j);
-	sum = S(imax,j,acc); cout << "* ";
+      else {//nothing changes
+	Simaxj += A[j];
+	Simaxj %= m;
       }
-      Simaxj += A[j]; Simaxj %= m;
-      cout << imax << ", " << j << " Simaxj: " << Simaxj << " sum: " << sum << endl;
+      if (Simaxj > Smax) {
+	maxSj = make_pair(imax,j);
+	Smax = Simaxj;
+	cout << imax << "," << j << " Smax: " << Smax << endl;
+      }
     }
     cout << maxSj.first << ", " <<  maxSj.second << " => ";
-    cout << S(maxSj.first, maxSj.second, acc) << endl;
+    cout << S(maxSj.first, maxSj.second, acc) << " " << Smax << endl;
     /*
     for (long j= 1; j<=50; j++) 
       for (long i= j;i<=50; i++) 
