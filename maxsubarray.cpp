b10e89cf7f57abc1199f4d9babb6727d3f344d52
frits-scholer <frits.scholer@gmail.com>
2016-08-01 14:46:27 +0200

    redid maxsubarray analysis

--- maxsubarray.cpp
+++ maxsubarray.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 typedef pair<long, long> subarray;
-#define S(i,j,B,mod) (B[j]-B[i-1]+mod)%mod
 
 int main() {
   long q;
@@ -16,21 +15,25 @@ int main() {
     A[0]=0;
     for (auto j=1;j<=n;j++) cin >> A[j];
     long long acc[n+1];
-    subarray maxSj = make_pair(1LL,1LL);
-    long long Simaxj=A[1]%m;
+    subarray maxSj = make_pair(1L,1L);
+    long long Siminj=A[1]%m;
+    long long Smax = A[1]%m;
     long imax=1;
     acc[0]=A[0];
     acc[1]=A[1]%m;
     for (long j=2;j<=n;j++) {
-      acc[j]=(acc[j-1]+A[j])%m;//beware of negatives
-      long long sum = S(maxSj.first, maxSj.second, acc, m);
-      if ((Simaxj + A[j])%m < A[j]%m) {
-	Simaxj = 0;
+      A[j] %= m;
+      acc[j]=(acc[j-1]+A[j])%m;
+      //assume Siminj is determined
+      if (acc[j-1] < Siminj) {//determine imax and Siminj
+	Siminj = acc[j-1];
 	imax = j;
       }
-      if ((A[j] + Simaxj)%m > sum) maxSj = make_pair(imax, j);
-      Simaxj = (Simaxj + A[j])%m;
+      if ((acc[j]-Siminj+m)%m > Smax) {
+	maxSj = make_pair(imax,j);
+	Smax = (acc[j]-Siminj+m)%m;
+      }
     }
-    cout << S(maxSj.first, maxSj.second, acc, m) << endl;
+    cout << Smax << endl;
   }
 }
--- testmaxsub.cpp
+++ testmaxsub.cpp
@@ -2,50 +2,34 @@
 #include <utility>
 using namespace std;
 
-typedef pair<long, long> subarray;
-#define S(i,j,B) ((B[j]-B[i-1]+m)%m)
-
 int main() {
   long q;
   cin >> q;
-  for (long i=0;i<q;i++) {
+  for (long iq=0;iq<q;iq++) {
     long n;//2<=n<=10^5
     long long m;//1<=m<=10^14
     cin >> n >> m;
     long long A[n+1];
     A[0]=0;
     for (auto j=1;j<=n;j++) cin >> A[j];
+    /*
+maxSij = max ((acc[j]-acc[i-1]%m)
+if acc[i-1] <= acc[j] maxSij = acc[j] (-min(acc[i-1]) which is 0)
+if acc[i-1] > acc[j] maxSij = acc[j]+m-min(acc[i-1])
+     */    
     long long acc[n+1];
-    subarray maxSj = make_pair(1L,1L);
-    long long Simaxj=A[1]%m;
     long long Smax = A[1]%m;
-    long imax=1;
-    acc[0]=A[0];
+    acc[0]=0;
     acc[1]=A[1]%m;
+    long long maxSij = acc[1];
     for (long j=2;j<=n;j++) {
       A[j] %= m;
-      acc[j]=(acc[j-1]+A[j])%m;//beware of negatives
-      //assume Simaxj is determined
-      if (Simaxj + A[j] >= m) {//determine imax and Simaxj
-	Simaxj = A[j];
-	imax = j;
-      }
-      else {//nothing changes
-	Simaxj += A[j];
-	Simaxj %= m;
-      }
-      if (Simaxj > Smax) {
-	maxSj = make_pair(imax,j);
-	Smax = Simaxj;
-	cout << imax << "," << j << " Smax: " << Smax << endl;
-      }
+      acc[j]=(acc[j-1]+A[j])%m;
+      //find delta the m-min(acc[i-1]) such that acc[i-1] > acc[j] 1<=i<j
+      long long delta = 0;
+      maxSij = acc[j]+delta;
+      if (maxSij>Smax) Smax = maxSij;
     }
-    cout << maxSj.first << ", " <<  maxSj.second << " => ";
-    cout << S(maxSj.first, maxSj.second, acc) << " " << Smax << endl;
-    /*
-    for (long j= 1; j<=50; j++) 
-      for (long i= j;i<=50; i++) 
-	cout << j << "," << i << ":" << S(j,i,acc)%m << endl;
-    */
+    cout << Smax << endl;
   }
 }
